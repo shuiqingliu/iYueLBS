0db821d7b1a3da2d921eb1c7dc4bc959b043456e
xxxifan <pm935938@gmail.com>
2015-02-02 00:02:31 +0800

    update AppConfig

--- app/src/main/java/com/iyuelbs/app/AppConfig.java
+++ app/src/main/java/com/iyuelbs/app/AppConfig.java
@@ -5,6 +5,7 @@ package com.iyuelbs.app;
  */
 public class AppConfig {
     public static final String BMOB_APP_KEY = "b03b3ba6c4c585e57496bd3a1dd00a23";
+    public static final String BMOB_REST_KEY = "6046cbcd9eb678cc99e298bed5a53c08";
     public static final String BMOB_AK = "e906767109cb49cc6400a4ec510cba36";
     public static final String WEIBO_KEY = "2729685708";
     public static final String WEIBO_SECRET = "79f1d519042f2b1cf609970a876add5a";
