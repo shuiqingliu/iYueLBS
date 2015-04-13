9b9d374fd32f3b4c34dd63b6af25ccdfada1436c
qingliu <shuiqingliu14@gmail.com>
2015-04-13 18:58:49 +0800

    用户界面半成品

--- app/src/main/java/com/iyuelbs/ui/main/MyInfoDetail.java
+++ app/src/main/java/com/iyuelbs/ui/main/MyInfoDetail.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +16,10 @@ import com.iyuelbs.support.widget.RoundedImageView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.display.SimpleBitmapDisplayer;
+import com.poliveira.parallaxrecycleradapter.ParallaxRecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by qingliu on 3/4/15.
@@ -36,17 +39,68 @@ public class MyInfoDetail extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_ditail);
-        setUserHead();
+        Log.e("start", "开始加载用户头像");
+        /*setUserHead();*/
 
         for (int i = 0;i<100;i++) {
             mDataset[i] = "fuck" + i + "次";
         }
         mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
-        //mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MyAdapter(mDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(manager);
+        //尝试滑动
+        final List<String> content = new ArrayList<>();
+        for (int i = 0; i < 30; i++)
+            content.add(getListString(i));
+
+        ParallaxRecyclerAdapter<String> stringAdapter = new ParallaxRecyclerAdapter<>(content);
+        stringAdapter.implementRecyclerAdapterMethods(new ParallaxRecyclerAdapter.RecyclerAdapterMethods() {
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+                /*((TextView) viewHolder.itemView).setText(content.get(i));*/
+            }
+
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+                return new SimpleViewHolder(getLayoutInflater().inflate(R.layout.card_view, viewGroup, false));
+            }
+
+            @Override
+            public int getItemCount() {
+                return content.size();
+            }
+        });
+        Log.e("header", "这里没错");
+        stringAdapter.setParallaxHeader(getLayoutInflater().inflate(R.layout.my_header, mRecyclerView, false), mRecyclerView);
+        Log.e("head", "有没有挂?");
+        stringAdapter.setOnParallaxScroll(new ParallaxRecyclerAdapter.OnParallaxScroll() {
+            @Override
+            public void onParallaxScroll(float percentage, float offset, View parallax) {
+                //TODO: implement toolbar alpha. See README for details
+            }
+        });
+
+
+        /*mAdapter = new MyAdapter(mDataset);*/
+        mRecyclerView.setAdapter(stringAdapter);
+        /*mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                ViewHelper.setTranslationY(findViewById(R.id.my_recycler_view),dy);
+            }
+        });*/
+    }
+    public String getListString(int postion) {
+        return postion + "- android";
+    }
+
+    static  class SimpleViewHolder extends RecyclerView.ViewHolder{
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 
     private void setUserHead(){
@@ -57,6 +111,7 @@ public class MyInfoDetail extends BaseActivity {
 
     private void userInfo(){
         User user = AppHelper.getCurrentUser();
+        Log.e("image", "开始getImageLoader");
         getImageLoader().displayImage(user.getAvatarUrl(), mAvatarImage, mImageOptions);
         mAvatarImage.setVisibility(View.VISIBLE);
         mUserNameText.setText(user.getNickName());
@@ -68,9 +123,9 @@ public class MyInfoDetail extends BaseActivity {
             mImageLoader = AppHelper.getImageLoader();
             mImageOptions = AppHelper.getDefaultOptsBuilder().displayer(new SimpleBitmapDisplayer()).build();
         }
+        Log.e("aaa","加载完毕");
         return mImageLoader;
     }
-
     @Override
     protected void setupWindowStyle() {
         super.setupWindowStyle();
@@ -83,12 +138,10 @@ public class MyInfoDetail extends BaseActivity {
 
     @Override
     protected void initFragments(Bundle savedInstanceState) {
-
     }
 
     @Override
     protected void initView() {
-
     }
 
     @Override
@@ -97,7 +150,7 @@ public class MyInfoDetail extends BaseActivity {
         return true;
     }
 
-    public class MyAdapter extends  RecyclerView.Adapter<MyAdapter.ViewHolder>{
+  /*  public class MyAdapter extends  RecyclerView.Adapter<MyAdapter.ViewHolder>{
         private String[] mDataset;
 
         public MyAdapter(String[] mDataset){
@@ -129,5 +182,5 @@ public class MyInfoDetail extends BaseActivity {
         public int getItemCount() {
             return mDataset.length;
         }
-    }
+    }*/
 }
